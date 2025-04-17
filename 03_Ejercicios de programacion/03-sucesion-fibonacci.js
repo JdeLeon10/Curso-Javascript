5f35eba2b2798416c0bfb5355bd43d18840c5d0f
Jeremy <jere.deleon8520@gmail.com>
2025-04-16 19:50:48 -0600

    Resolucion ejercicio de sucesion de fibonacci

--- 03_Ejercicios de programacion/03-sucesion-fibonacci.js
+++ 03_Ejercicios de programacion/03-sucesion-fibonacci.js
@@ -5,3 +5,16 @@
  *   la que el siguiente siempre es la suma de los dos anteriores.
  *   0, 1, 1, 2, 3, 5, 8, 13...
  */
+
+let primerValor = 0; // Primer valor de la serie Fibonacci
+let segundoValor = 1; // Segundo valor de la serie Fibonacci
+let siguienteValor; // Siguiente valor de la serie Fibonacci
+
+let cantidadValores = 50; // Cantidad de valores a imprimir
+
+for (let i = 0; i < cantidadValores; i++) {
+    console.log(primerValor); // Imprimir el valor actual
+    siguienteValor = primerValor + segundoValor; // Calcular el siguiente valor
+    primerValor = segundoValor; // Actualizar el primer valor
+    segundoValor = siguienteValor; // Actualizar el segundo valor
+}
